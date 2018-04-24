5f2a36f671d66226ceeec2e98d00d932e69d5aa2
veveve10 <32618477+veveve10@users.noreply.github.com>
2018-04-24 07:44:46 -0300

    Add files via upload

--- Genetico/main.cpp
+++ Genetico/main.cpp
@@ -3,7 +3,15 @@
 
 int main(void)
 {
-	std::cout << "Hello";
+	int popSize = 200;
+	int seed = 12; //valor a ser gerado de forma aleatoria
+	std::vector<bin> população;
+	for (int i = 0; i < popSize; i++)
+	{
+		população.push_back(leArq(""));
+		população[i] = reordena(população[i], seed);
+	}
+
 
 	return 0;
 }
