d838ae54d6ec778bd32457c8d35cae650b5e6680
yangurevitz <32618901+yangurevitz@users.noreply.github.com>
2018-05-07 09:41:20 -0300

    Comentarios

--- TrabIA/TrabIA/anealing.cpp
+++ TrabIA/TrabIA/anealing.cpp
@@ -17,41 +17,42 @@
 void simulatedAnnealing(char* Path, int n)
 {
 	printf("\n===================================================\n");
-	double e = 2.718281828, p, temp;
-	unsigned int saSeed, swapSeed;
+	double e = 2.718281828, p, temp; //valores para o calculo da probabilidade de aceitar movimento pior
+	unsigned int saSeed, swapSeed; //seeds de geracao de numeros aleatorios
 	int qtdIni = 1;
 	std::vector<Solucao> solu;
-	if (strcmp(ARQ1,Path) == 0)// Resultado: 20 em 0 seg
+	//segue o controle de qual instancia do problema se trata
+	if (strcmp(ARQ1,Path) == 0)
 	{
 		solu = criaVecPop(Path, qtdIni);
 		saSeed = 69322;
 		swapSeed = 23353;
 	}
-	else if (strcmp(ARQ2, Path) == 0)// Resultado: 41 em 4 seg
+	else if (strcmp(ARQ2, Path) == 0)
 	{
 		solu = criaVecPop(Path, qtdIni);
 		saSeed = 194562;
 		swapSeed = 13455;
 	}
-	else if (strcmp(ARQ3, Path) == 0)// Resultado: 46 em 87 seg
+	else if (strcmp(ARQ3, Path) == 0)
 	{
 		solu = criaVecPop(Path, qtdIni);
 		saSeed = 1762;
 		swapSeed = 16875;
 	}
-	else if (strcmp(ARQ4, Path) == 0)// Resultado: 104 em 106 seg
+	else if (strcmp(ARQ4, Path) == 0)
 	{
 		solu = criaVecPop(Path, qtdIni);
 		saSeed = 12;
 		swapSeed = 13;
 	}
-	else if (strcmp(ARQ5, Path) == 0)// Resultado: 213 em 137 seg
+	else if (strcmp(ARQ5, Path) == 0)
 	{
 		solu = criaVecPop(ARQ5, qtdIni);
 		saSeed = 151101;
 		swapSeed = 16200;
 	}
-	else
+	else //caso nao seja um arquivo valido
 	{
 		printf("Instancia nao existe\n");
 		exit(1);
@@ -59,34 +60,34 @@ void simulatedAnnealing(char* Path, int n)
 	printf("Primeira Solucao: %d\n", solu[0].getBins().size());
 
 	clock_t Ticks[2];
-	Ticks[0] = clock();
+	Ticks[0] = clock(); //guarda o tempo de inicio do algoritimo
 
 	for (int i = 0; i<solu.size(); i++)
 	{
-		for (temp = 80; temp>0.00008; temp *= 0.90)
+		for (temp = 80; temp>0.00008; temp *= 0.90) //controle de temperatura
 		{
-			for (unsigned int j = 0; j<n; j++)
+			for (unsigned int j = 0; j<n; j++) //controle de iteracoes para cada faixa de temperatura
 			{
 				auto soluNew = solu[i];
-				soluNew.swap(swapSeed);
-				int delta = fitness(soluNew) - fitness(solu[i]);
-				if (delta < 0)
+				soluNew.swap(swapSeed); //pega um vizinho da solucao corrente
+				int delta = fitness(soluNew) - fitness(solu[i]); //determina qual a melhor solucao e por quanto
+				if (delta < 0) //movimento de melhora
 				{
 					solu[i] = soluNew;
 				}
 				else
 				{
 					p = pow(e, -((double)delta) / temp);
-					if (rand() / (double)RAND_MAX <= p)
+					if (rand() / (double)RAND_MAX <= p) //possibilidade de piora
 						solu[i] = soluNew;
 				}
 			}
 			
 		}
 		printf("Solucao final: %d\n", solu[i].getBins().size());
-		solu[i].geraArq(Path, "Anealing");
+		solu[i].geraArq(Path, "Anealing"); //gera o arquivo para a solucao final
 	}
 	Ticks[1] = clock();
-	float Tempo = (Ticks[1] - Ticks[0]) / (float)CLOCKS_PER_SEC;
+	float Tempo = (Ticks[1] - Ticks[0]) / (float)CLOCKS_PER_SEC; //tempo total de execucao
 	printf("Tempo gasto: %.2f s.\n", Tempo);
 }
